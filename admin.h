2448efba8a070171e3bd403a247ba95ca313c233
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-09 21:33:33 +0800

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,12 +1,19 @@
 #ifndef ADMIN_H
 #define ADMIN_H
+#include <iostream>
+#include <string>
+using namespace std;
 
-class admin {
+class Admin
+{
 private:
-
-
-
+	string _UserId;
+	string _password;
 
 public:
-
+	string getUserId();
+	string getpassword();
+	void setUserId(string UserId);
+	void setpassword(string password);
 };
+#endif 
