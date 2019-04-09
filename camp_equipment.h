8351198e8100fbd0fabcecdbaf92e53d853d8369
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-09 23:19:41 +0800

    Update camp_equipment.h

--- camp_equipment.h
+++ camp_equipment.h
@@ -1,58 +1,38 @@
-#ifndef EQUIPMENT_H
-#define EQUIPMENT_H
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-class Equipment {
-private:
-	string itemCode;
-	string itemName;
-	string brand;
-	string itemType;
-	string dateOfPurchase;
-	string condition;
-	string status;
-
-public:
-	virtual void outputBasicInfo();
-	virtual void outputAttributInfo();
-	Equipment();							//	GENERAL Function
-	Equipment(string TypeOfEquipment);		//	SPECIFIC Function
+	Equipment(string _itemCode, string _itemName, string _brand,
+		string _itemType, int _dateOfPurchase, string _condition, string _status);
+	void outputBasicInfo(); 
+	bool isReturn();					//	GENERAL Function
+	
 };
 	
 class Tent : public Equipment {
 private:
-	int pplnum[6] = { 2,4,6,8,10,16 };  
-	string tentType[5] = {"lightweight", "family", "wall","tunnel","dome" };
-	int numberOfDoors[3] = {1,2,4};
+	int pplnum;
+	string tentType;
+	int numberOfDoors;
 	bool doubleLayer;
-	string colour[5] = {"blue", "orange", "yellow", "grey", "red"};
+	string colour;
 
 public:
-	virtual void outputBasicInfo();
-	virtual void outputAttributInfo();
+	
+	virtual void outputAttributeInfo();
 };
 class Stove : public Equipment {
 private:
-	string stoveType[3] = {"ultralight", "lightweight", "table"};
-	string fuelType[3] = {"stove gas", "screw headed gas", "wax"};
+	string stoveType;
+	string fuelType;
 
 public:
-	virtual void outputBasicInfo();
-	virtual void outputAttributInfo();
+	void outputAttributeInfo();
 };
 
 class Lantern : public Equipment {
 private:
-	string lanternSize[2] = {"hiking", "camping"};
-	string lanternType[4] = {"head lamp", "table lamp", "led lamp", "beam lamp" };
-	string fuelType[6] = { "AA batteries", "AAA batteries", "C batteries", 
-		"D batteries", "stove gas", "screw headed gas" };
+	string lanternSize;
+	string lanternType;
+	string fuelType;
+		
 public:
-	virtual void outputBasicInfo();
-	virtual void outputAttributInfo();
+	void outputAttributeInfo();
 };
 #endif
-
