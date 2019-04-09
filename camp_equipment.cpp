4cb20e5bd00482aaf49bea0007343963642a2922
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-09 23:19:18 +0800

    Update camp_equipment.cpp

--- camp_equipment.cpp
+++ camp_equipment.cpp
@@ -1,7 +1,39 @@
 #include "camp_equipment.h"
+#include <string>
 #include <iostream>
 using namespace std;
 
-void Tent::outputBasicInfo() {
-	
+Equipment::Equipment(string itemCode, string itemName, string brand,
+	string itemType, int dateOfPurchase, string condition, string status) {
+	_itemCode = itemCode;
+	_itemName = itemName;
+	_brand = brand;
+	_itemType = itemType;
+	_dateOfPurchase = dateOfPurchase;
+}
+
+void Equipment::outputBasicInfo()
+{
+	cout << _itemCode << "|" << _itemName << "|" << _brand << "|" << _itemType << "|" << _dateOfPurchase << "|" << _condition << "|" << _status << "|" << endl;
+}
+
+
+void Tent::outputAttributeInfo()
+{
+	cout << pplnum << "|" << tentType << "|" << numberOfDoors << "|" << doubleLayer << "|" << colour << endl;
+}
+
+
+void Stove::outputAttributeInfo()
+{
+	cout << stoveType << "|" << fuelType << endl;
+}
+
+void Lantern::outputAttributeInfo()
+{
+	cout << lanternSize << "|" << lanternType << "|" << fuelType << endl;
+}
+
+bool Equipment::isReturn() {
+	 _status == "in" ? true : false;
 }
