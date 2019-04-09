f12f62dfabe0505f1d32c8b054e05e023db776c2
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-09 21:34:01 +0800

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,1 +1,16 @@
-#include "admin.h"
+#include "Admin.h"
+string Admin::getUserId() {
+	return _UserId;
+}
+
+
+string Admin::getpassword() {
+	return _password;
+}
+
+void Admin::setUserId(string UserId) {
+	_UserId = UserId;
+}
+void Admin::setpassword(string password) {
+	_password = password;
+}
