d1dfc1cc0b1c7327154583148fe2098eb0fe7bfd
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-09 21:41:16 +0800

    Add files via upload

---
+++ loanControl.h
@@ -0,0 +1,17 @@
+#ifndef LOANCONTROL_H
+#define LOANCONTROL_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class LoanControl
+{
+public:
+	int numOfItems;
+
+public:
+	LoanControl()
+	
+
+};
+#endif 
