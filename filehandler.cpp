52365b34caaf6f59c070632a4eb06efdb748eb8c
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-12 15:34:19 +0800

    Update filehandler.cpp

--- filehandler.cpp
+++ filehandler.cpp
@@ -1,1 +1,42 @@
-// ho gor
+#include "filehandler.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string fileRead(string path) {
+
+	ifstream file(path, ios::in);
+
+
+	if (file.is_open()) {
+
+		stringstream ss;
+
+		ss << file.rdbuf();
+
+		return ss.str();
+
+	}
+
+
+	return NULL;
+
+}
+
+
+void fileWrite(string path, string str) {
+
+	ofstream file(path, ios::out);
+
+
+	if (file.is_open()) {
+
+		stringstream ss;
+
+		ss << str;
+
+		file << ss.rdbuf();
+
+	}
+
+}
