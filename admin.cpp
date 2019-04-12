a04452bc96b278932ad2fff6e6bf34a7260c5457
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-12 15:32:10 +0800

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,27 +1,33 @@
-#include "Admin.h"
-#include<iostream>
-string Admin::getUserId() {
-	return _UserId;
-}
+#include "filehandler.h"		//	insert tabulated files
+#include "camp_equipment.h"		// display list of camp equipment
+#ifndef ADMIN_H
+#define ADMIN_H
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
-string Admin::getpassword() {
-	return _password;
-}
+class Admin
+{
+private:
+	string _UserId;
+	string _password;
+	int _loanDate;
+	string _itemCode;
+	string _itemName;
+	string _NameOfBorrower;
+	int _returnDate;
+	string _status;
 
-void Admin::setUserId(string UserId) {
-	_UserId = UserId;
-}
-void Admin::setpassword(string password) {
-	_password = password;
-}
+public:
+	string getUserId();
+	string getpassword();
+	void setUserId(string UserId);
+	void setpassword(string password);
+	Admin();
+	void loanRecord(int _loanDate, string _itemCode, string _itemName,
+		string _NameOfBorrower, int _returnDate, string _status);
+	void displayloanRecord();
 
-Admin::Admin() {
-	_UserId = "admin";
-	_password = "admin";
-}
-
-bool Admin::isAdminLogin(string UserId, string password) {
-	bool access = true;
-
-	access = ((_UserId == UserId) && (_password == password)) ? true : false;
-}
+};
+#endif 
