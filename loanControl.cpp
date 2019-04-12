f72ccd0253455a6bd28fb7c89f2772830fb148d9
JacobHui <44830229+JacobHui@users.noreply.github.com>
2019-04-12 22:30:34 +0800

    Update loanControl.cpp

--- loanControl.cpp
+++ loanControl.cpp
@@ -1,1 +1,17 @@
-#include
+#include "LoanControl.h"
+#include <iostream>
+
+using namespace std;
+
+
+bool loancontrol::check(Users x, Equipments y)
+{
+	if (x.getStatus() && y.getStatus())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
