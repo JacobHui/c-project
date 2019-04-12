6fb639b8c17e39129e17f6273da0e51011d12c18
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-12 15:34:38 +0800

    Update filehandler.h

--- filehandler.h
+++ filehandler.h
@@ -1,1 +1,17 @@
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+class fileHandler
+{
+private:
+	
+public:
+	string fileRead(string path);
+	void fileWrite(string path, string str);
+};
+
+#endif
